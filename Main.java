21cf087b793149623496613dc4dbe318ca44fa32
Kasid1n <kasidinkhamfoo@gmail.com>
2026-01-30 15:57:46 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+void main() {
+    Character J = new Character(100,50);
+    J.swingSword();
+    J.swingSword();
+    J.swingSword();
+    J.swingSword();
+    J.swingSword();
+    J.takeDamage(30);
+    J.takeDamage(50);
+    J.takeDamage(50);
+    J.rest();
+    J.displayStatus();
+}
